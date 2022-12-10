7166d81147d5e35fe86a16aa4803a1fcb0beccf6
Barajas <carmen-895@hotmail.com>
2022-12-10 16:38:09 -0500

    Comienzo desafio

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        include: path.resolve(__dirname, "src"),
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: 'assets/[name].[ext]'
+            }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+  ],
+  output: {
+    chunkFilename: "[name].[contenthash].js",
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
