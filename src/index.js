2f433d153243b0c9e2b77db98e11be70a9558379
Barajas <carmen-895@hotmail.com>
2022-12-22 15:02:05 -0500

    Importando imagenes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import './css/style.css';
+
+
+
+import './assets/javascript.svg';
+import './assets/webpack.svg';
+import './assets/html.svg';
+import './assets/css.svg';
+import './assets/tailwind-css.svg';
+import './assets/bootstrap.svg';
+import './assets/series.png';
+import './assets/pintar.png';
+import './assets/mucica.svg';
+import './assets/logo-alura.svg';
+import './assets/logo-udemy.svg';
+import './assets/Platzi.jpg';
+import './assets/leonidas.jpg';
+import './assets/logo-google-activate.png';
+import './assets/Logo-sena.png';
+import './assets/Logo-sena.png';
+import './assets/encriptador-imagen.png';
+import './assets/imagen-ahorcado.svg';
+import './assets/contacto.svg';
 
 
 
